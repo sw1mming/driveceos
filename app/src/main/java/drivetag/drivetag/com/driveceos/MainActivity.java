fbed51652e1fb2772e59a7ffa10cba8d5cd2ccac
yuriy <yura4057@gmail.com>
2017-03-27 13:07:44 +0300

    Implemented "UpdateCoverPhotoRequest".

--- app/src/main/java/drivetag/drivetag/com/driveceos/MainActivity.java
+++ app/src/main/java/drivetag/drivetag/com/driveceos/MainActivity.java
@@ -6,6 +6,7 @@ import android.support.annotation.Nullable;
 
 import drivetag.drivetag.com.driveceos.data_layer.requests.ServerRequest;
 import drivetag.drivetag.com.driveceos.data_layer.requests.on_boarding.SignInRequest;
+import drivetag.drivetag.com.driveceos.data_layer.requests.user_profile.UpdateCoverPhotoRequest;
 
 /**
  * Created by yuriy on 3/22/17.
@@ -18,17 +19,35 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        SignInRequest signInRequest = new SignInRequest("rayban@mailinator.com", "qwe123456");
-        signInRequest.resumeWithCompletionHandler(new SignInRequest.ServerCompletionHandler() {
-            @Override
-            public void completionHandler(ServerRequest request) {
-                System.out.println();
-            }
+//        SignInRequest signInRequest = new SignInRequest("rayban@mailinator.com", "qwe123456");
+//        signInRequest.resumeWithCompletionHandler(new SignInRequest.ServerCompletionHandler() {
+//            @Override
+//            public void completionHandler(ServerRequest request) {
+//                System.out.println();
+//            }
+//
+//            @Override
+//            public void completionHandlerWithError(String error) {
+//                System.out.println();
+//            }
+//        });
 
-            @Override
-            public void completionHandlerWithError(String error) {
-                System.out.println();
-            }
-        });
+
+//
+//          UpdateCoverPhotoRequest updateCoverPhotoRequest = new UpdateCoverPhotoRequest(null);
+//          updateCoverPhotoRequest.resumeWithCompletionHandler(new UpdateCoverPhotoRequest.ServerCompletionHandler() {
+//            @Override
+//            public void completionHandler(ServerRequest request) {
+//                System.out.println();
+//            }
+//
+//            @Override
+//            public void completionHandlerWithError(String error) {
+//                System.out.println();
+//            }
+//        });
+
+
+
     }
 }
