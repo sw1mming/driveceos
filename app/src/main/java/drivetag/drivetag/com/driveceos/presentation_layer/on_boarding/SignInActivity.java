b8cdffee440b63597a087fb7eb13ae56ea3431ca
Artem Vasilchikov <Artem-qz@mail.ru>
2017-03-27 14:47:16 +0300

    Implemented UI part of Sign In screen.

---
+++ app/src/main/java/drivetag/drivetag/com/driveceos/presentation_layer/on_boarding/SignInActivity.java
@@ -0,0 +1,20 @@
+package drivetag.drivetag.com.driveceos.presentation_layer.on_boarding;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+import drivetag.drivetag.com.driveceos.BaseActivity;
+import drivetag.drivetag.com.driveceos.R;
+
+
+/**
+ * Created by artem on 3/24/17.
+ */
+
+public class SignInActivity extends BaseActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_in);
+    }
+}
