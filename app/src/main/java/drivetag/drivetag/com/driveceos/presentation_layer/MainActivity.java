ec65ed2a062f5445ecdf046b117a3e994581f952
alex <ryabuh.alexandr@gmail.com>
2017-04-02 19:59:31 +0300

    - create splash activity - create forgot password activity

--- app/src/main/java/drivetag/drivetag/com/driveceos/presentation_layer/MainActivity.java
+++ app/src/main/java/drivetag/drivetag/com/driveceos/presentation_layer/MainActivity.java
@@ -1,206 +1,47 @@
 package drivetag.drivetag.com.driveceos.presentation_layer;
 
-import android.app.Activity;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.TextView;
 
 import drivetag.drivetag.com.driveceos.R;
 
-/**
- * Created by yuriy on 3/22/17.
- */
+public class MainActivity extends AppCompatActivity {
 
-public class MainActivity extends Activity {
+    private TextView mTextMessage;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+                    mTextMessage.setText(R.string.title_home);
+                    return true;
+                case R.id.navigation_dashboard:
+                    mTextMessage.setText(R.string.title_dashboard);
+                    return true;
+                case R.id.navigation_notifications:
+                    mTextMessage.setText(R.string.title_notifications);
+                    return true;
+            }
+            return false;
+        }
+
+    };
 
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        /**  SocialNetworkEnableRequest  */
-//        SocialNetworkEnableRequest socialNetworkEnableRequest = new SocialNetworkEnableRequest(SocialNetworkFacebook);
-//        socialNetworkEnableRequest.resumeWithCompletionHandler(new ServerRequest.ServerCompletionHandler() {
-//            @Override
-//            public void completionHandler(ServerRequest request) {
-//                System.out.println();
-//            }
-//
-//            @Override
-//            public void completionHandlerWithError(String error) {
-//                System.out.println();
-//            }
-//        });
-
-
-
-        /**  LoadCurrentUserRequest  */
-//        FacebookLoginRequest facebookLoginRequest = new FacebookLoginRequest("EAAUmQyOW8JMBAHi13J33hOqZBkUpPtHBHXVgb0TgNqGBFGZByRDzuiqpvJiZAez2Q0ZBtgXgx8v52vlwtDTyj377ZC7iTxZAQXOq2XtHazpGf8QJMsgCP7uB9qZBDFJSMZAbvZCoGkloZAr6RK7yxxqT4wpdOHijQRsWhxAxshICk54mxCBQiIoYFhyEH70sfKS2iv7HmpjICl2Km7OPRYaZAIeyfmGAHJr0vMZD", "869147083162811");
-//        facebookLoginRequest.resumeWithCompletionHandler(new ServerRequest.ServerCompletionHandler() {
-//            @Override
-//            public void completionHandler(ServerRequest request) {
-//                System.out.println();
-//            }
-//
-//            @Override
-//            public void completionHandlerWithError(String error) {
-//                System.out.println();
-//            }
-//        });
-
-
-        /**  LoadCurrentUserRequest  */
-//        LoadCurrentUserRequest loadCurrentUserRequest = new LoadCurrentUserRequest();
-//        loadCurrentUserRequest.resumeWithCompletionHandler(new ServerRequest.ServerCompletionHandler() {
-//            @Override
-//            public void completionHandler(ServerRequest request) {
-//                System.out.println();
-//            }
-//
-//            @Override
-//            public void completionHandlerWithError(String error) {
-//                System.out.println();
-//            }
-//        });
-
-
-        /**  EmployeeTitlesRequest  */
-//        EmployeeTitlesRequest request = new EmployeeTitlesRequest("rayban@mailinator.com");
-//        request.resumeWithCompletionHandler(new ServerRequest.ServerCompletionHandler() {
-//            @Override
-//            public void completionHandler(ServerRequest request) {
-//                System.out.println();
-//            }
-//
-//            @Override
-//            public void completionHandlerWithError(String error) {
-//                System.out.println();
-//            }
-//        });
-
-        /**  VerifyRequest  */
-//        VerifyRequest verifyRequest = new VerifyRequest("safcvafceqwcwcqqewc@gmail.com");
-//        verifyRequest.resumeWithCompletionHandler(new VerifyRequest.ServerCompletionHandler() {
-//            @Override
-//            public void completionHandler(ServerRequest request) {
-//                System.out.println();
-//            }
-//
-//            @Override
-//            public void completionHandlerWithError(String error) {
-//                System.out.println();
-//            }
-//        });
-
-
-        /**  CheckEmailTypeRequest  */
-//        CheckEmailTypeRequest checkEmailTypeRequest = new CheckEmailTypeRequest("safcvafceqwcwcqqewc@gmail.com");
-//        checkEmailTypeRequest.resumeWithCompletionHandler(new CheckEmailTypeRequest.ServerCompletionHandler() {
-//            @Override
-//            public void completionHandler(ServerRequest request) {
-//                System.out.println();
-//            }
-//
-//            @Override
-//            public void completionHandlerWithError(String error) {
-//                System.out.println();
-//            }
-//        });
-
-
-        /**  SuggestedIdentifierRequest  */
-//        SuggestedIdentifierRequest suggestedIdentifierRequest = new SuggestedIdentifierRequest("dcefd3Secc@mailinator.com", "dcefd3Secc","Dfvcdsdfgfd");
-//        suggestedIdentifierRequest.resumeWithCompletionHandler(new SuggestedIdentifierRequest.ServerCompletionHandler() {
-//            @Override
-//            public void completionHandler(ServerRequest request) {
-//                System.out.println();
-//            }
-//
-//            @Override
-//            public void completionHandlerWithError(String error) {
-//                System.out.println();
-//            }
-//        });
-
-
-        /**  ImageUploadRequest  */
-//        Drawable myIcon = getResources().getDrawable( R.drawable.facebook_enabled_icon );
-//
-//        ImageUploadRequest imageUploadRequest = new ImageUploadRequest(myIcon);
-//        imageUploadRequest.resumeWithCompletionHandler(new ImageUploadRequest.ServerCompletionHandler() {
-//            @Override
-//            public void completionHandler(ServerRequest request) {
-//                System.out.println();
-//            }
-//
-//            @Override
-//            public void completionHandlerWithError(String error) {
-//                System.out.println();
-//            }
-//        });
-
-
-//        String fileName = String.format("text_%d", 1);
-//                System.out.println(fileName);
-//        System.out.println(fileName);
-
-
-//        log.debug( String.format("%s is %d years old, er, young", "Al", 45) );
-
-//        Intent intent = new Intent(this, UserProfileActivity.class);
-//        startActivity(intent);
-
-//        SignInRequest signInRequest = new SignInRequest("rayban@mailinator.com", "qwe123456");
-//        signInRequest.resumeWithCompletionHandler(new SignInRequest.ServerCompletionHandler() {
-//            @Override
-//            public void completionHandler(ServerRequest request) {
-//                System.out.println();
-//            }
-//
-//            @Override
-//            public void completionHandlerWithError(String error) {
-//                System.out.println();
-//            }
-//        });
-
-        /**  UpdateCoverPhotoRequest  */
-//
-//          UpdateCoverPhotoRequest updateCoverPhotoRequest = new UpdateCoverPhotoRequest(null);
-//          updateCoverPhotoRequest.resumeWithCompletionHandler(new UpdateCoverPhotoRequest.ServerCompletionHandler() {
-//            @Override
-//            public void completionHandler(ServerRequest request) {
-//                System.out.println();
-//            }
-//
-//            @Override
-//            public void completionHandlerWithError(String error) {
-//                System.out.println();
-//            }
-//        });
-
-        /**  UpdateMyPageRequest  */
-
-//
-//        List<String> array = new ArrayList<>();
-//        array.add("https://www.rover.com/blog/wp-content/uploads/2015/05/dog-candy-junk-food-599x340.jpg");
-//
-//
-//          UpdateMyPageRequest updateMyPageRequest = new UpdateMyPageRequest(array);
-//          updateMyPageRequest.resumeWithCompletionHandler(new UpdateMyPageRequest.ServerCompletionHandler() {
-//            @Override
-//            public void completionHandler(ServerRequest request) {
-
-//        MyTeamupRequest request = new MyTeamupRequest();
-//        request.resumeWithCompletionHandler(new MyTeamupRequest.MyTeamupCompletionHandler<List<User>, List<User>>() {
-//
-//            @Override
-//            public void completionHandler(List<User> historyUsers, List<User> incomingUsers, ServerRequest request) {
-//                System.out.println();
-//            }
-//
-//            @Override
-//            public void completionHandlerWithError(String error) {
-//                System.out.println();
-//            }
-//        });
+        mTextMessage = (TextView) findViewById(R.id.message);
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
+
 }
