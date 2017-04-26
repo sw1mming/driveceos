8125c6dcb7a208d215b39f02a1981cc502bf5772
Artem Vasilchikov <Artem-qz@mail.ru>
2017-04-26 17:25:05 +0300

    Implemented "People Driving" data source.

--- app/src/main/java/drivetag/drivetag/com/driveceos/helpers/Constants.java
+++ app/src/main/java/drivetag/drivetag/com/driveceos/helpers/Constants.java
@@ -10,4 +10,9 @@ public abstract class Constants {
         public static final String KEY_DL_SHARED_PREFERENCE = "KEY_DL_SHARED_PREFERENCE";
         public static final String THOUGHT_TYPE_NONE = "none";
     }
+
+    public enum PeopleDrivingType {
+        PEOPLE_DRIVING_TYPE_POST,
+        PEOPLE_DRIVING_TYPE_TODAY
+    }
 }
