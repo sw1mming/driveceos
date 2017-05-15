4199947104c0d450d212d888539202ac6b897256
yuriy <yura4057@gmail.com>
2017-05-15 11:54:05 +0300

    Implemented "FollowRequest".

---
+++ app/src/main/java/drivetag/drivetag/com/driveceos/data_layer/requests/post/FollowRequest.java
@@ -0,0 +1,90 @@
+package drivetag.drivetag.com.driveceos.data_layer.requests.post;
+
+import com.google.gson.JsonElement;
+
+import drivetag.drivetag.com.driveceos.data_layer.models.Tag;
+import drivetag.drivetag.com.driveceos.data_layer.requests.ServerRequest;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.http.DELETE;
+import retrofit2.http.Headers;
+import retrofit2.http.PUT;
+import retrofit2.http.Path;
+
+/**
+ * Created by YuriySokurko on 5/15/17.
+ */
+
+public class FollowRequest extends ServerRequest {
+
+    public Tag tag;
+    private FollowApi service;
+
+
+    /**
+     * Interface.
+     */
+
+    public FollowRequest(Tag tag) {
+        this.tag = tag;
+    }
+
+    public void resumeWithCompletionHandler(final ServerCompletionHandler handler) {
+        Call<JsonElement> call = null;
+        if (tag.isFollowing == 0) {
+            call = service.follow(tag.identifier);
+        } else {
+            call = service.followDelete(tag.identifier);
+        }
+
+        call.enqueue(new Callback<JsonElement>() {
+            @Override
+            public void onResponse(Call<JsonElement> call, Response<JsonElement> response) {
+                handleSuccessResponse(response);
+                handler.completionHandler(getFollowRequest());
+            }
+
+            @Override
+            public void onFailure(Call<JsonElement> call, Throwable t) {
+                handler.completionHandlerWithError(t.toString());
+            }
+        });
+
+        if (tag.isFollowing == 1) {
+            tag.isFollowing = 0;
+        } else {
+            tag.isFollowing = 1;
+        }
+    }
+
+    @Override
+    public void setupService() {
+        Retrofit retrofit = getRetrofit();
+        service = retrofit.create(FollowApi.class);
+    }
+
+
+    /** Private. */
+
+    private FollowRequest getFollowRequest() {
+        return this;
+    }
+
+
+    /** FollowApi interface. */
+
+    interface FollowApi {
+
+        @Headers({
+                "Authorization: Basic MTQ3NTE2NzMyMDM5NS04OTQ4MDI4ODU5NDUxNDE3MDc3Og==",
+        })
+
+        @PUT("api/v2/tags/{tagId}/follow")
+        Call<JsonElement> follow(@Path("tagId") Integer identifier);
+
+        @DELETE("api/v2/tags/{tagId}/follow")
+        Call<JsonElement> followDelete(@Path("tagId") Integer identifier);
+    }
+}
